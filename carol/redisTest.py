172229bd0d5cf69040b61a735913605d2649fec9
Q-Mart <qumarthjash@gmail.com>
2019-01-07 18:03:40 +0000

    Messing around with docker-compose and redis

--- carol/redisTest.py
+++ carol/redisTest.py
@@ -1,5 +1,11 @@
 import redis
 
 PORT=6379
+PASSWORD = None
 
-r = redis.Redis(host='redis', port=PORT)
+with open('conf/password') as f:
+    PASSWORD = f.read().strip()
+
+r = redis.Redis(host='redis', port=PORT, password=PASSWORD)
+print(r.get('foo'))
+print (r.save())
