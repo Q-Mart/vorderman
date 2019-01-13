474d7fc107c3008cc3b99b41b100ffdb38e95148
Q-Mart <qumarthjash@gmail.com>
2019-01-13 15:57:34 +0000

    Working letters round API

--- carol/app.py
+++ carol/app.py
@@ -1,33 +1,9 @@
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify
 
 import lettersRound
 import numbersRound
 
 app = Flask(__name__)
-
-# class SolveLetters(Resource):
-#     def get(self, inputLetters):
-#         if len(inputLetters) > 9:
-#             result = {'error':
-#                         {'status': '400',
-#                          'title': 'Input length greater than 9'}
-#                       }
-#         elif len(inputLetters) == 0:
-#             result = {'error':
-#                         {'status': '400',
-#                          'title': 'No input'}
-#                       }
-#         else:
-#             result = {'error': {'status': '200'},
-#                       'data':
-#                         {'solutions': [sol for sol in lettersround.solve(inputLetters)]}
-#                       }
-
-#         return jsonify(result)
-
-# class Hello(Resource):
-#     def get(self):
-#         return jsonify({'data': 'Hello world!'})
 
 @app.route('/')
 def index():
@@ -36,30 +12,21 @@ def index():
 @app.route('/letters/<inputLetters>', methods=['GET'])
 def solve_letters(inputLetters):
     if len(inputLetters) > 9:
-        # result = {'error':
-        #             {'status': '400',
-        #              'title': 'Input length greater than 9'}
-        #           }
-        errorData = json.dumps({'status': '400', 'title': 'Input length greater than 9'})
-        return jsonify(error=errorData)
+        result = {'error':
+                    {'status': '400',
+                     'title': 'Input length greater than 9'}
+                  }
     elif len(inputLetters) == 0:
-        # result = {'error':
-        #             {'status': '400',
-        #              'title': 'No input'}
-        #           }
-        errorData = json.dumps({'status': '400', 'title': 'No input'})
-        return jsonify(error=errorData)
+        result = {'error':
+                    {'status': '400',
+                     'title': 'No input'}
+                  }
     else:
-        # result = {'error': {'status': '200'},
-        #           'data':
-        #             {'solutions': [sol for sol in lettersround.solve(inputLetters)]}
-        #           }
-        errorData = json.dumps({'status': '200'})
-        payloadData = json.dumps({'solutions': [sol for sol in lettersRound.solve(inputLetters)]})
-        return jsonify(error=errorData, data=payloadData)
-
-# api.add_resource(Hello, '/')
-# api.add_resource(SolveLetters, '/letters/<inputLetters>')
+        result = {'error': {'status': '200'},
+                  'data':
+                    {'solutions': [sol.decode('utf-8') for sol in lettersRound.solve(inputLetters)]}
+                  }
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
